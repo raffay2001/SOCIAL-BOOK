4519df64058dfc0785f1fa229b7c45cdf4f10132
ABDUL RAFFAY <raffay.ahmed009@gmail.com>
2022-12-18 02:01:58 +0500

    feat: update user account settings done

--- core/urls.py
+++ core/urls.py
@@ -2,8 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # home page route
     path('', views.index, name='index'),
+    # authentication routes
     path('signup', views.signup, name='signup'),
     path('signin', views.signin, name='signin'),
-    path('logout', views.logout, name='logout')
+    path('logout', views.logout, name='logout'),
+    # account settings route
+    path('settings', views.settings, name='settings')
 ]
--- core/views.py
+++ core/views.py
@@ -38,11 +38,14 @@ def signup(request):
                 user.save()
 
                 # log user in and redirect to settings page
+                user_login = authenticate(username=username, password=password)
+                login(user_login)
+
                 # create a profile object for the new user
                 user_model = User.objects.get(username=username)
                 new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
                 new_profile.save()
-                return HttpResponseRedirect(reverse('signup'))
+                return HttpResponseRedirect(reverse('settings'))
         else:
             messages.info(request, 'Password Not Matching.')
             return HttpResponseRedirect(reverse('signup'))
@@ -70,3 +73,31 @@ def signin(request):
 def logout(request):
     Logout(request)
     return HttpResponseRedirect(reverse('signin'))
+
+# Controller for the settings page
+@login_required(login_url='signin')
+def settings(request):
+    user_profile = Profile.objects.get(user=request.user)
+    if request.method == 'POST':
+        if request.FILES.get('image') == None:
+            image = user_profile.profileimg
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+        if request.FILES.get('image') != None:
+            image = request.FILES.get('image')
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+
+        return HttpResponseRedirect(reverse('settings'))
+
+    return render(request, 'setting.html', context={'user_profile':user_profile})
