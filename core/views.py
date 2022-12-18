888c23369ce9918673cb4591e501f2341ac1c49d
ABDUL RAFFAY <raffay.ahmed009@gmail.com>
2022-12-18 19:22:07 +0500

    chore: styling and other fixes

--- core/views.py
+++ core/views.py
@@ -7,6 +7,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib import messages
 from .models import *
 from itertools import chain
+import random
 
 # Create your views here.
 
@@ -24,8 +25,29 @@ def index(request):
         feed_lists = Post.objects.filter(user=usernames)
         feed.append(feed_lists)
     feed_list = list(chain(*feed))
-    posts = Post.objects.all()
-    return render(request, 'index.html', {'user_profile': user_profile_object, 'posts': feed_list})
+
+    # user suggestions start
+    all_users = User.objects.all()
+    user_following_all = []
+
+    for user in user_following:
+        user_list = User.objects.get(username=user.user)
+        user_following_all.append(user_list)
+
+    new_suggestions_list = [x for x in list(all_users) if (x not in list(user_following_all))]
+    current_user = User.objects.filter(username=request.user.username)
+    final_suggestions_list = [x for x in list(new_suggestions_list) if (x not in list(current_user))]
+    random.shuffle(final_suggestions_list)
+    username_profile = []
+    username_profile_list = []
+    for users in final_suggestions_list:
+        username_profile.append(users.id)
+    for ids in username_profile:
+        profile_lists = Profile.objects.filter(id_user=ids)
+        username_profile_list.append(profile_lists)
+    suggestions_username_profile_list = list(chain(*username_profile_list))
+
+    return render(request, 'index.html', {'user_profile': user_profile_object, 'posts': feed_list, 'suggestions_username_profile_list': suggestions_username_profile_list[:4]})
 
 # Controller for the signup page
 def signup(request):
